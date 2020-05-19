6e83e5a8d2b572d71ee77ae32d7732928d5d2930
Omolola <53899116+motunrayo01@users.noreply.github.com>
2020-05-19 00:22:30 -0300

    number counter
    when a button is clicked, numbers increases or decreases and change color according to the button clicked

---
+++ js/counter.js
@@ -0,0 +1,12 @@
+function Lower() {
+    var lower=1
+    
+document.getElementById(`template`).innerHTML -=lower;
+
+document.getElementById(`template`).style.color=`red`;
+ }     
+function add() {
+    var add=0
+document.getElementById(`template`).innerHTML ++;
+document.getElementById(`template`).style.color=`green`;
+}
